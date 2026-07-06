29801d7ee6906510569a8fa6110ca8938785952c
basingwaebasing-hash <basingwaebasing@gmail.com>
2026-07-06 21:04:37 +0700

    Update footer to credit Cloudflare instead of Rin

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -102,7 +102,7 @@ function Footer() {
                             setDoubleClickTimes(doubleClickTimes + 1)
                         }
                     }}>
-                        © {new Date().getFullYear()} Powered by <a className='hover:underline' href="https://github.com/openRin/Rin" target="_blank">Rin</a>
+                        © {new Date().getFullYear()} Powered by <a className='hover:underline' href="https://cloudflare.com/" target="_blank">Cloudflare</a>
                     </span>
                     {config.getBoolean('rss') && <>
                         <Spliter />
